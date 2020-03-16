4196441b7013045a5d0e52385de4689265dfc67b
antikrem <30370708+antikrem@users.noreply.github.com>
2020-03-16 22:29:49 +1000

    Added initial files

---
+++ bot.js
@@ -0,0 +1,164 @@
+
+// Stores the state of the bot, including lists of channels, banend users, e.g.
+var state = require("./state");
+const { Client, MessageAttachment } = require('discord.js')
+
+// Functional components
+
+// Obfuscates a given message, deleting the original
+function obfuscateMessage(message) {
+    var embeds = message.embeds;
+    var attachments = message.attachments.array();
+    var channel = message.channel
+
+    console.log("\nObfuscation Begins:")
+    console.log("attachments: " + message.attachments.size)
+    
+    var textBody = "";
+
+    // Handle message content, storing in textBody
+    if (message.content != "") {
+       // If there is no user id, then create one
+       if ( !(state.userToUUID.has(message.author.id)) ) {
+            state.userToUUID.set(message.author.id, state.generateUUID())
+        }
+
+        //post a message of text
+        textBody = "`UserID: " + state.userToUUID.get(message.author.id) +'`\n'
+                 + message.content;
+    }    
+
+    // Handle attachments  
+    var attachmentUrls = attachments.map(m => m.url);
+    
+    var imagePromise = null;
+
+    // Post each image, which requires image urls to be valid
+    for (var i = 0; i < attachmentUrls.length; i++) {
+        var UUID = state.generateUUID();
+        state.imageUUIDToPoster.set(UUID, message.author.id);
+        imagePromise = channel.send("`ID: " + UUID + "`\n", {file: attachmentUrls[i]});
+        console.log("Image posted");
+        console.log("Image " + (i+1) + " of " + attachments.length + " posted by [REDACTED]")
+    }
+
+    // Delete message
+    if (imagePromise) {
+        // If theres an image promise, require
+        // waiting for image post before deleting
+        imagePromise.then(
+            function(value) {
+                message.delete();
+            }
+        );
+    }
+    else {
+        message.delete();
+    }
+    
+    // If there is a some text to post, send it
+    if (textBody != "") {
+        channel.send(textBody);
+    }   
+}
+
+// Handles a command message posted in a channe;
+function handleCommand(message) {
+    let args = message.content.substr(1).split(" ");
+    let cmd = args[0].toLowerCase();
+    let prm = args.slice(1);
+    let isMod = state.isUserMod(message.author);
+    
+    console.log("Cmd recieved: " + args);
+    console.log("idMod?: " + isMod);
+    
+    switch (cmd) {
+        case "add" :
+        state.addChannel(message.channel);
+        message.channel.send("The veil covers this channel")
+        break;
+
+        case "changeid" :
+        state.resetID(message.author);
+        break;
+
+        case "changeallids" :
+        state.resetAllID();
+        break;
+
+        case "ban" :
+        if (prm.length > 0 && isMod) {
+            if (state.banUser(prm[0])) {
+                message.channel.send("User has been banned from posting")
+            } else {
+                message.channel.send("UUID not found")
+            }
+        }
+        break;
+
+        case "bandump" :
+        state.dumpAllBans();        
+        break;
+    }
+
+    message.delete();
+}
+
+const client = new Client()
+
+// Event Handlers
+
+// General event handler
+client.on('message', (message) => {
+    //Catch self message case, a simple parrot and leave
+    if (message.author == client.user) {
+    }
+
+    //server text channel messages will be treated as expected
+    else if (message.channel.type == 'text') {
+        // Handle banned user
+        if (state.isUserBanned(message.author.id)) {
+            message.author.send("You have been banned, and your message deleted");
+            message.delete();
+        }
+        //otherwise handle commands and messages differently
+        else {
+            if (message.content.startsWith(state.cmdToken)) {
+                handleCommand(message);
+            }
+            else if (state.isChannelActive(message.channel) && !state.isUserBanned(message.author)) {
+                obfuscateMessage(message);
+            }
+            else {
+                if (message.author.dmChannel) {
+                message.author.dmChannel.send("You have been banned from using the veil")
+                }
+                message.delete();
+            }
+            
+        }
+        
+    }    
+})
+
+// Handles first load in
+client.on('ready', () => {
+    console.clear();
+    console.log("Connected as " + client.user.tag);
+})
+
+// Handles an error event, usually
+// a small disconnect or internet interrupt
+client.on('error', () => {
+    console.log("An error event has occured");
+})
+
+// Timer to reset time every day
+setInterval(
+    function (){
+        state.resetAllID();
+    }, 
+    state.resetTime * 60 * 60 * 1000
+);
+
+client.login(state.getLoginToken())
---
+++ state.js
@@ -0,0 +1,142 @@
+/* Contains the "state" of the bot
+ * Which is a collections of conatiners
+ * with information on users, chanells, etc 
+ * Also includes some features that are hard coded in
+ */
+
+var fs = require('fs');
+
+// Token for commands
+module.exports.cmdToken = '%'
+
+// Array of active channels
+module.exports.activeChannels = []
+
+// Array of bans
+module.exports.bannedUsers = []
+
+// A list of signitures associated with admins:
+module.exports.mods = ["chally#4215", "ehryus#2741", "Lewen#8592"]
+
+// Ties a uuid of image post to user id
+module.exports.imageUUIDToPoster = new Map();
+
+// Ties a user id to UUID
+module.exports.userToUUID = new Map();
+
+// Reset time in hours
+module.exports.resetTime = 24;
+
+// Generates a UUID, of 8 random letters (case-sensitive)
+function generateUUID() {
+    var UUID = "";
+    for (var i = 0; i < 8; i++) {
+        if (Math.random() < 0.5) {
+            UUID += String.fromCharCode(Math.floor(Math.random()*25+65));
+        } 
+        else {
+            UUID += String.fromCharCode(Math.floor(Math.random()*25+97));
+        }
+    }    
+    return UUID;
+}
+
+module.exports.generateUUID = generateUUID;
+
+const tokenFile = './login_token'
+// Gets the token for logging in
+function getLoginToken() {
+    try {
+        var file = fs.readFileSync(tokenFile);
+        return file.toString();
+    } catch {
+        console.log("Error: no token file found, expected a file \"" + tokenFile + "\" with token for use");
+        process.exit(1);
+    }
+}
+
+module.exports.getLoginToken = getLoginToken;
+
+// Returns true is channel is active
+function isChannelActive(channel) {
+    return module.exports.activeChannels.includes(channel.id);
+}
+
+module.exports.isChannelActive = isChannelActive;
+
+// Adds current channel into activeChannels
+// No effect is already in active channel
+function addChannel(channel) {
+    if (!module.exports.activeChannels.includes(channel.id)) {
+        module.exports.activeChannels.push(channel.id);
+    }
+}
+
+module.exports.addChannel = addChannel;
+
+// Returns a unique signiture for each person
+function getSig(user) {
+    return user.username+'#'+user.discriminator
+}
+
+module.exports.getSig = getSig;
+
+// Returns true if user is banned
+function isUserBanned(user) {
+    return module.exports.bannedUsers.includes(user.id);
+}
+
+module.exports.isUserBanned = isUserBanned;
+
+// tries to ban a user by uuid, return false on failure
+function banUser(uuid) {
+    // Check image posters
+    if (module.exports.imageUUIDToPoster.has(uuid)) {
+        module.exports.bannedUsers.push(module.exports.imageUUIDToPoster.get(uuid))
+        return true;
+    }
+
+    // Check text users
+    for (let [n, id] of module.exports.userToUUID) {
+        if (id == uuid) {
+            module.exports.bannedUsers.push(n)
+            return true;
+        }
+     }
+    
+    return false;
+}
+
+module.exports.banUser = banUser;
+
+// Returns true if user is a mod
+function isUserMod(user) {
+    return module.exports.mods.includes(getSig(user));
+}
+
+module.exports.isUserMod = isUserMod;
+
+// Resets a users UUID
+function resetID(user) {
+    module.exports.userToUUID.set(user.id, generateUUID())
+}
+
+module.exports.resetID = resetID;
+
+// Resets all UUID 
+function resetAllID() {
+    for (let [user, id] of module.exports.userToUUID) {
+        module.exports.userToUUID.set(user, generateUUID())    
+    }
+}
+
+module.exports.resetAllID = resetAllID;
+
+// Dump all banned id
+function dumpAllBans() {
+    for (let i = 0; i < module.exports.bannedUsers.length; i++) {
+        console.log(module.exports.bannedUsers.length[i])
+    }   
+}
+
+module.exports.dumpAllBans = dumpAllBans;
