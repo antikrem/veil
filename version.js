2ba2e1159270c8591f02bfb688768f288b73b10c
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-04-10 00:22:02 +1000

    Finalised proxy posting

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 // Used to keep version number
 
 module.exports.VERSION_RELEASE = 0
-module.exports.VERSION_MAJOR = 2
-module.exports.VERSION_MINOR = 4
+module.exports.VERSION_MAJOR = 3
+module.exports.VERSION_MINOR = 0
